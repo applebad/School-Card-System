c083cf197ff465c6aecf32e98b61920687447e58
applebad <116998672+applebad@users.noreply.github.com>
2024-06-24 22:52:37 +0800

    new 22:52

--- TransactionDatas.java
+++ TransactionDatas.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 
 public class TransactionDatas {
-    
+    private static Map<String,ArrayList<Transaction>> transactionMap = null;
+
     /**
      * 移除卡号交易记录
      * @param cno 卡号
@@ -19,15 +19,19 @@ public class TransactionDatas {
      * 保存交易记录数据
      */
     public void saveTransaction() {
-       
+        TransactionFiles transactionFiles = new TransactionFiles();
+        transactionFiles.saveTransactionMap((Map<String, ArrayList<Transaction>>) transactionMap);
     }
     /**
      * 获取交易记录
      * @return
      */
     public static Map<String,ArrayList<Transaction>> getTransactions() {
-        TransactionFiles transactionFiles = new TransactionFiles();
-        return transactionFiles.transactionsMap;
+        if(transactionMap==null){
+            TransactionFiles transactionFiles = new TransactionFiles();//通过构造方法获取文件中的数据
+            transactionMap = transactionFiles.getTransactionMap();//存到内存
+        }
+        return transactionMap;
     }
     /**
      * 清空交易记录
--- TransactionFiles.java
+++ TransactionFiles.java
@@ -7,10 +7,9 @@ public class TransactionFiles {
     Map<String,ArrayList<Transaction>> transactionsMap = new HashMap<String,ArrayList<Transaction>>();
 
     File[] files = null;
-
+    File file = new File("data\\transactions\\");
     public TransactionFiles(){
         try {
-            File file = new File("data\\transactions\\");
             this.files = file.listFiles();//文件名
         } catch (Exception e) {
             e.printStackTrace();
@@ -24,7 +23,24 @@ public class TransactionFiles {
     }
 
     public void removeTransactionByCno(String cno) {
+        File data = new File("data\\transactions\\"+cno+".dat");
+        if(data.exists() &&data.isFile()){//是文件则删除
+            data.delete();
+        }
         transactionsMap.remove(cno);
+    }
+
+    public void saveTransaction(String cno,ArrayList<Transaction> transactions){
+        TransactionFile transactionFile = new TransactionFile(cno);
+        transactionFile.save(transactions);
+    }
+
+    public void saveTransactionMap(Map<String,ArrayList<Transaction>> newMap){
+        transactionsMap = newMap;
+    }
+
+    public Map<String,ArrayList<Transaction>> getTransactionMap(){
+        return transactionsMap;
     }
     
 }
